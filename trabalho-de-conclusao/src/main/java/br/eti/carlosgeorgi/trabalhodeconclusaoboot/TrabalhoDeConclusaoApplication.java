c2b4ea2cd907c9b0fe2460f068b1e3b2b29e9de5
Carlos Henrique <Carlos Henrique@CarlosHenrique>
2018-11-02 15:30:55 -0200

    Commit Template Projeto

---
+++ trabalho-de-conclusao/src/main/java/br/eti/carlosgeorgi/trabalhodeconclusaoboot/TrabalhoDeConclusaoApplication.java
@@ -0,0 +1,12 @@
+package br.eti.carlosgeorgi.trabalhodeconclusaoboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoDeConclusaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoDeConclusaoApplication.class, args);
+	}
+}
---
+++ trabalho-de-conclusao/src/test/java/br/eti/carlosgeorgi/trabalhodeconclusaoboot/TrabalhoDeConclusaoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.eti.carlosgeorgi.trabalhodeconclusaoboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrabalhoDeConclusaoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
