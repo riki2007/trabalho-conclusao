2316638bb34ca30d9152d95fa0fa700dd604761c
Carlos Henrique Leite <carlosh_leite@hotmail.com>
2018-11-16 00:03:42 -0200

    ;..

--- trabalho-de-conclusao/src/main/java/br/eti/carlosgeorgi/trabalhodeconclusaoboot/web/controller/ProdutoController.java
+++ trabalho-de-conclusao/src/main/java/br/eti/carlosgeorgi/trabalhodeconclusaoboot/web/controller/ProdutoController.java
@@ -1,19 +1,35 @@
 package br.eti.carlosgeorgi.trabalhodeconclusaoboot.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import br.eti.carlosgeorgi.trabalhodeconclusaoboot.domain.Produto;
+import br.eti.carlosgeorgi.trabalhodeconclusaoboot.service.ProdutoService;
 
 @Controller
 @RequestMapping("/produtos")
 public class ProdutoController {
+	
+	@Autowired
+	private ProdutoService service;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar(){
+	public String cadastrar(Produto produto){
 		return "/produtos/cadastro";
 	}
 	
 	@GetMapping("/listar")
 	public String listar(){
 		return "/produtos/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Produto produto) {
+		service.salvar(produto);
+		return "redirect:/produtos/cadastrar";
 	}
 }
